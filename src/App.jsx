442cdf40f8254dc45f795952d48a5e8f3f8d32ed
mclevins <mclevins@gmail.com>
2025-02-12 13:26:50 -0500

    final edits to travel journal

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,20 @@
 import Header from './components/Header';
 import Entry from './components/Entry';
 import Contact from './components/Contact';
+import data from './data';
 
 export default function App() {
+    const entryElements = data.map(entry => {
+        return <Entry 
+            key={entry.id}
+            {...entry}
+        />
+    });
     return (
         <>
             <Header/>
             <ul className="entry-list">
-                <Entry
-                    name="Mount Fuji"
-                    dates="12 Jan, 2021 - 24 Jan, 2021"
-                    description="Mount Fuji is the tallest mountain in Japan, standing at 3,776 meters 
-                        (12,380 feet). Mount Fuji is the single most popular tourist
-                            site in Japan, for both Japanese and foreign tourists."
-                    img="https://scrimba.com/links/travel-journal-japan-image-url"
-                    alt="picture of Japan"
-                    country="Japan"
-                    href="https://www.google.com/maps/place/Mount+Fuji/@35.3606421,138.7170637,15z/data=!3m1!4b1!4m6!3m5!1s0x6019629a42fdc899:0xa6a1fcc916f3a4df!8m2!3d35.3606255!4d138.7273634!16zL20vMGNrczA?entry=ttu"
-                />
+                {entryElements}
             </ul>
         </>   
     );
--- src/components/Entry.jsx
+++ src/components/Entry.jsx
@@ -5,17 +5,17 @@ export default function Entry(props) {
         <div className="journal-container">
             <article className="journal-entry">
                 <div className="image-container">
-                    <img src={props.img} className="entry-image" alt={props.alt} />
+                    <img src={props.img.src} className="entry-image" alt={props.img.alt} />
                 </div>
                 <div className="info-container">
                     <img src={marker} className="marker" alt="marker" />
                     <span className="country inter weight-400 spaced upper">{props.country}</span>
                     <a 
-                    href="https://www.google.com/maps/place/Mount+Fuji/@35.3606421,138.7170637,15z/data=!3m1!4b1!4m6!3m5!1s0x6019629a42fdc899:0xa6a1fcc916f3a4df!8m2!3d35.3606255!4d138.7273634!16zL20vMGNrczA?entry=ttu"
+                    href={props.googleMapsLink}
                     className="map-link inter weight-400">View on Google Maps</a>
-                    <h1 className="entry-title inter weight-700">{props.name}</h1>
+                    <h1 className="entry-title inter weight-700">{props.title}</h1>
                     <p className="trip-dates inter weight-700">{props.dates}</p>
-                    <p className="entry-text inter weight-400">{props.description}</p>                
+                    <p className="entry-text inter weight-400">{props.text}</p>                
                 </div>
             </article>
         </div>
